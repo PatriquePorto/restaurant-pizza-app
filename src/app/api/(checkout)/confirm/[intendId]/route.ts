2b272c01670ff7232e4b8e2203774e534c11ec2f
Patrique Porto <patrikrock@hotmail.com>
2023-09-29 10:22:38 -0300

    Vercel deploy config

--- src/app/api/(checkout)/confirm/[intendId]/route.ts
+++ src/app/api/(checkout)/confirm/[intendId]/route.ts
@@ -1,5 +1,5 @@
 //import { PrismaClient } from "@prisma/client";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from './prisma/generated/client'
 import { NextRequest, NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
